b333daa8cb7be1612e0cbc1c9954922aadb7e5d3
2214yj <2214yjgm@gmail.com>
2022-01-03 02:13:33 +0900

    UserController validation 추가

--- api-server-spring-boot-final/src/main/java/com/example/demo/utils/ValidationRegex.java
+++ api-server-spring-boot-final/src/main/java/com/example/demo/utils/ValidationRegex.java
@@ -12,6 +12,13 @@ public class ValidationRegex {
         return matcher.find();
     }
 
+    public static boolean isRegexPassword(String target) {
+        String regex = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,20}$"; //영문,숫자,특수문자
+        Pattern pattern = Pattern.compile(regex,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(target);
+        return matcher.find();
+    }
+
     // 날짜 형식, 전화 번호 형식 등 여러 Regex 인터넷에 검색하면 나옴.
 }
 
